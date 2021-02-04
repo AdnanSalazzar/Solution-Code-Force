39f861d29ef8a9e56697eebfe85c34b4af416726
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2021-02-04 22:36:31 +0600

    Add files via upload

---
+++ A. Omkar and Completion.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int T , i , N ;
+    cin >> T ;
+
+    while(T--)
+    {
+        cin >> N ;
+
+        for(i = 0 ; i < N  ; i++)
+        {
+            cout << 1 << " " ;
+        }
+        cout << endl;
+    }
+    return 0 ;
+}
