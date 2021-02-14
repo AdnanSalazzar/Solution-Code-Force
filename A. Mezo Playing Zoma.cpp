dee98c2ef237fc9b25962b3640c1c6d9a5f42c2e
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2021-02-14 19:07:41 +0600

    Add files via upload

---
+++ A. Mezo Playing Zoma.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std  ;
+
+int main()
+{
+    int N ;
+    string a ;
+
+    cin >> N ;
+    getchar();
+    cin >> a ;
+
+    cout << N+1 <<endl;
+
+    return 0 ;
+
+}
