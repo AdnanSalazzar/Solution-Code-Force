7893ae6ebdc65036a4d8b44952db424245b13d92
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2021-04-09 17:54:45 +0600

    Create 	959A - Mahmoud and Ehab and the even-odd game.cpp

---
+++ 959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int num ;
+    cin >>  num ;
+
+    if( num & 1 )
+        cout << "Ehab" <<endl;
+
+    else
+        cout << "Mahmoud" <<endl;
+
+
+    return 0 ;
+}
