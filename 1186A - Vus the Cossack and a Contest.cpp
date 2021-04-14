d40fdae55a1fc35696d6328cc0c3a3e4c6885a72
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2021-04-14 16:56:31 +0600

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ 1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<list>
+#include<iterator>
+#include<algorithm>
+#include<cmath>
+using namespace std ;
+
+int main()
+{
+    int n , m , k ;
+
+    cin >> n >> m >>k ;
+
+    if( n <= k && n <= m)
+    {
+        cout <<"YES"<<endl;
+    }
+
+    else
+    {
+        cout << "NO" <<endl;
+    }
+
+
+return 0 ;
+}
+
