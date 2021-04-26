2eeace6fe52152971bce57f0cb419594f1cce8b3
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2021-04-26 23:38:33 +0600

    Create 1325A - EhAb AnD gCd.cpp

---
+++ 1325A - EhAb AnD gCd.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<list>
+#include<iterator>
+#include<algorithm>
+#include<cmath>
+#include<string.h>
+
+#define AMAI ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+
+using namespace std ;
+
+int main()
+{
+    AMAI;
+    int T , x;
+
+    cin >> T ;
+
+    while(T--)
+    {
+        cin >> x ;
+
+        cout << 1 << " " << --x <<endl;
+
+    }
+
+
+return 0 ;
+}
+
+
