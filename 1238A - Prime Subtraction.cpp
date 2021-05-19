f39c27147087e3559aea0d6b48c056b4582f8b8f
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2021-05-20 00:02:29 +0600

    Create 1238A - Prime Subtraction.cpp

---
+++ 1238A - Prime Subtraction.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<list>
+#include<iterator>
+#include<algorithm>
+#include<cmath>
+#include<string.h>
+#include<vector>
+
+#define longpro int64_t ;
+#define AMAI ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+
+using namespace std ;
+
+int main()
+{
+    AMAI;
+
+    int T ;
+
+    cin >> T ;
+    while(T--)
+    {
+
+        ll a, b ;
+
+        cin >> a>> b ;
+
+        if(a-1 == b)
+        {
+            cout << "NO" <<endl;
+        }
+        else
+        {
+            cout << "YES" <<endl;
+        }
+
+    }
+
+
+    return 0 ;
+}
+
+
